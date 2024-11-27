71c1191b76cb922cdf26a71977840bcd4c2a2e2f
Allen Porter <allen.porter@gmail.com>
2024-11-27 18:36:46 +0000

    Move main functions out of init

--- mcp_server_home_assistant/__init__.py
+++ mcp_server_home_assistant/__init__.py
@@ -1,30 +1,3 @@
 """
 .. include:: ../README.md
 """
-
-import click
-import logging
-import sys
-from .server import serve
-
-
-@click.command()
-@click.option("--url", required=True, help="Home Assistant websocket url")
-@click.option("--token", required=True, help="Home Assistant token")
-@click.option("-v", "--verbose", count=True)
-def main(url: str, token: str, verbose: bool) -> None:
-    """MCP Home Assistant Server."""
-    import asyncio
-
-    logging_level = logging.WARN
-    if verbose == 1:
-        logging_level = logging.INFO
-    elif verbose >= 2:
-        logging_level = logging.DEBUG
-
-    logging.basicConfig(level=logging_level, stream=sys.stderr)
-    asyncio.run(serve(url, token))
-
-
-if __name__ == "__main__":
-    main()
--- mcp_server_home_assistant/main.py
+++
@@ -1,3 +0,0 @@
-from mcp_server_home_assistant import main
-
-main()
