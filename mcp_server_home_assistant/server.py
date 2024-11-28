017a0871ddf889c66dcddb19423f3e36e1aea0b8
Allen Porter <allen.porter@gmail.com>
2024-11-27 17:44:11 -0800

    Fixes to return resources and tools

--- mcp_server_home_assistant/server.py
+++ mcp_server_home_assistant/server.py
@@ -1,10 +1,12 @@
 """MCP server for Home Assistant."""
 
+import asyncio
 import logging
 
 from aiohttp import ClientSession
 
 from hass_client.client import HomeAssistantClient
+from hass_client.exceptions import BaseHassClientError
 from mcp.server import Server
 from mcp.server.stdio import stdio_server
 from mcp.types import (
@@ -24,10 +26,31 @@ async def create_server(
     client = HomeAssistantClient(url, token, aiohttp_session)
     await client.connect()
 
+    async def listener() -> None:
+        """Start listening on the HA websockets."""
+        try:
+            # start listening will block until the connection is lost/closed
+            await client.start_listening()
+        except BaseHassClientError as err:
+            _LOGGER.warning("Connection to HA lost due to error: %s", err)
+        _LOGGER.info(
+            "Connection to HA lost. Connection will be automatically retried later."
+        )
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(listener())
+
     @server.list_tools()  # type: ignore[no-untyped-call, misc]
     async def list_tools() -> list[Tool]:
         results = await client.send_command("mcp/tools/list")
-        tools = [Tool(**result) for result in results["tools"]]
+        tools = [
+            Tool(
+                name=result["name"],
+                description=result.get("description"),
+                inputSchema=result.get("input_schema"),
+            )
+            for result in results["tools"]
+        ]
         _LOGGER.debug("Returning %d tools", len(tools))
         return tools
 
@@ -38,18 +61,18 @@ async def create_server(
 
     @server.list_resources()  # type: ignore[no-untyped-call, misc]
     async def list_resources() -> list[dict]:
-        _LOGGER.debug("Listing resources")
         results = await client.send_command("mcp/resources/list")
         resources = results["resources"]
         _LOGGER.debug("Returning %d resources", len(resources))
-        return resources
+        return resources  # type: ignore[no-any-return]
 
     @server.read_resource()  # type: ignore[no-untyped-call, misc]
     async def read_resource(uri: str) -> dict:
-        results = await client.send_command("mcp/resources/read", uri=uri)
+        results = await client.send_command("mcp/resources/read", uri=str(uri))
         contents = results["contents"]
-        _LOGGER.debug("Returning %d contents", len(contents))
-        return contents
+        content = contents[0]["text"]
+        _LOGGER.debug("Returning resource %s of length %s", uri, content)
+        return content  # type: ignore[no-any-return]
 
     return server
 
